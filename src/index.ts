88a0312db9a0c6333600cbba8cb189efc7d51b2c
Shahzod-webdev <bahodurovshahzod84@gmail.com>
2024-01-16 14:42:31 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from'express'
+
+
+const app = express()
+const port = 3000
+
+const jsonBodyMiddleware = express.json()
+
+app.use(jsonBodyMiddleware)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
